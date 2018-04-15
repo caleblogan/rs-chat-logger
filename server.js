3b672e93addba2e0687e74161783d4a6a06d68b8
caleb logan <clogan202@gmail.com>
2018-04-15 14:03:01 -0700

    add PORT config

--- server.js
+++ server.js
@@ -32,4 +32,5 @@ app.use(http404Handler);
 
 app.use(errorHandler);
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
